3c227a19e464487c8d0d155b9d289d5aa965a093
KaPiech <58893947+KaPiech@users.noreply.github.com>
2020-11-03 14:53:55 +0100

    Signed-off-by: KaPiech <58893947+KaPiech@users.noreply.github.com>

--- Mnożenie_macierzy.py
+++ Mnożenie_macierzy.py
@@ -5,12 +5,16 @@ import numpy as np
 random.seed()
 A = np.random.randn(8, 8)
 B = np.random.randn(8, 8)
+C = np.zeros((8, 8))
 
-C = A.dot(B)
+for i in range(len(A)):
+       for j in range(len(B[0])):
+           for k in range(len(B)):
+               C[i][j] += A[i][k] * B[k][j]
 
-print("Macierz A:")
+print("Macierz A =")
 print(A)
-print("Macierz B:")
+print("Macierz B =")
 print(B)
-print("Macierz C = A * B")
+print("Macierz C = A * B =")
 print(C)
